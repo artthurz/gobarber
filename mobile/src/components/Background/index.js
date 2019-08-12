ab1c9519eccb6590db6d5f60bca5a4a5ad8f5d1f
Arthur Hauenstein <52018251+artthurz@users.noreply.github.com>
2019-08-12 10:35:56 -0300

    mobile

---
+++ mobile/src/components/Background/index.js
@@ -0,0 +1,10 @@
+import LinearGradient from 'react-native-linear-gradient';
+import styled from 'styled-components/native';
+
+export default styled(LinearGradient).attrs({
+  // colors: ['#060a16', '#696c73'],
+  // colors: ['#7159c1', '#ab59c1'],
+  colors: ['#4169e1', '#7159c1'],
+})`
+  flex: 1;
+`;
