e801ade9e17877551bc458f0b91e057ecdb91ce1
Arthur Hauenstein <arthur_hauenstein@fruki.com.br>
2019-11-25 20:48:42 -0300

    ajustes

--- desktop/src/pages/Configuration/index.js
+++ desktop/src/pages/Configuration/index.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
+import { toast } from 'react-toastify';
+import { parseISO, getHours, getMinutes } from 'date-fns';
 import { Container } from './styles';
 
+import api from '~/services/api';
+
 export default function Configuration() {
-  const [openMorning, setOpenMorning] = useState();
-  const [closeMorning, setCloseMorning] = useState();
-  const [openAfternoon, setOpenAfternoon] = useState();
-  const [closeAfternoon, setCloseAfternoon] = useState();
+  const [ms, setMorning_start] = useState();
+  const [me, setMorning_end] = useState();
+  const [as, setAfternoon_start] = useState();
+  const [ae, setAfternoon_end] = useState();
   const [sunday, setSunday] = useState();
   const [monday, setMonday] = useState();
   const [tuesday, setTuesday] = useState();
@@ -15,8 +19,28 @@ export default function Configuration() {
   const [friday, setFriday] = useState();
   const [saturday, setSaturday] = useState();
 
-  function handleSubmit() {
-    return null;
+  async function handleSubmit() {
+    const morning_start = `${getHours(ms)}:${getMinutes(ms)}`;
+    const morning_end = `${getHours(me)}:${getMinutes(me)}`;
+    const afternoon_start = `${getHours(as)}:${getMinutes(as)}`;
+    const afternoon_end = `${getHours(ae)}:${getMinutes(ae)}`;
+    const data = {
+      sunday,
+      monday,
+      tuesday,
+      wednesday,
+      thursday,
+      friday,
+      saturday,
+      morning_start,
+      morning_end,
+      afternoon_start,
+      afternoon_end,
+    };
+
+    console.log(data);
+    await api.put(`/configuration/horary/${1}`, data);
+    toast.success('Horários de funcionamento atualizados com sucesso!');
   }
 
   return (
@@ -25,132 +49,138 @@ export default function Configuration() {
       <aside>
         <span>* Horário abertura manhã </span>
         <DatePicker
-          selected={openMorning}
-          onChange={date => setOpenMorning(date)}
-          dateFormat="dd/MM/y"
+          selected={ms}
+          onChange={date => setMorning_start(date)}
+          showTimeSelect
+          showTimeSelectOnly
+          dateFormat="h:mm aa"
           placeholder="Data de Nascimento"
         />
       </aside>
       <aside>
         <span>* Horário fechamento manhã </span>
         <DatePicker
-          selected={closeMorning}
-          onChange={date => setCloseMorning(date)}
-          dateFormat="dd/MM/y"
+          selected={me}
+          onChange={date => setMorning_end(date)}
+          showTimeSelect
+          showTimeSelectOnly
+          dateFormat="h:mm aa"
           placeholder="Data de Nascimento"
         />
       </aside>
       <aside>
         <span>* Horário abertura tarde </span>
         <DatePicker
-          selected={openAfternoon}
-          onChange={date => setOpenAfternoon(date)}
-          dateFormat="dd/MM/y"
+          selected={as}
+          onChange={date => setAfternoon_start(date)}
+          showTimeSelect
+          showTimeSelectOnly
+          dateFormat="h:mm aa"
           placeholder="Data de Nascimento"
         />
       </aside>
       <aside>
         <span>* Horário fechamento tarde </span>
         <DatePicker
-          selected={closeAfternoon}
-          onChange={date => setCloseAfternoon(date)}
-          dateFormat="dd/MM/y"
+          selected={ae}
+          onChange={date => setAfternoon_end(date)}
+          showTimeSelect
+          showTimeSelectOnly
+          dateFormat="h:mm aa"
           placeholder="Data de Nascimento"
         />
       </aside>
       <hr />
-      <div>
-        <span>Domingo</span>
-        <label className="switch">
-          <input
-            name="sunday"
-            type="checkbox"
-            placeholder="Checkbox"
-            onChange={e => setSunday(e.target.checked)}
-          />
-          <div className="slider" />
-        </label>
-      </div>
-
-      <div>
-        <span>Segunda</span>
-        <label className="switch">
-          <input
-            name="monday"
-            type="checkbox"
-            placeholder="Checkbox"
-            onChange={e => setMonday(e.target.checked)}
-          />
-          <div className="slider" />
-        </label>
-      </div>
-
-      <div>
-        <span>Terça</span>
-        <label className="switch">
-          <input
-            name="tuesday"
-            type="checkbox"
-            placeholder="Checkbox"
-            onChange={e => setTuesday(e.target.checked)}
-          />
-          <div className="slider" />
-        </label>
-      </div>
-
-      <div>
-        <span>Quarta</span>
-        <label className="switch">
-          <input
-            name="wednesday"
-            type="checkbox"
-            placeholder="Checkbox"
-            onChange={e => setWednesday(e.target.checked)}
-          />
-          <div className="slider" />
-        </label>
-      </div>
-
-      <div>
-        <span>Quinta</span>
-        <label className="switch">
-          <input
-            name="thursday"
-            type="checkbox"
-            placeholder="Checkbox"
-            onChange={e => setThursday(e.target.checked)}
-          />
-          <div className="slider" />
-        </label>
-      </div>
-
-      <div>
-        <span>Sexta</span>
-        <label className="switch">
-          <input
-            name="friday"
-            type="checkbox"
-            placeholder="Checkbox"
-            onChange={e => setFriday(e.target.checked)}
-          />
-          <div className="slider" />
-        </label>
-      </div>
-
-      <div>
-        <span>Sábado</span>
-        <label className="switch">
-          <input
-            name="saturday"
-            type="checkbox"
-            placeholder="Checkbox"
-            onChange={e => setSaturday(e.target.checked)}
-          />
-          <div className="slider" />
-        </label>
-      </div>
+      <ul>
+        <div>
+          <span>Domingo:</span>
+          <label className="switch">
+            <input
+              name="sunday"
+              type="checkbox"
+              placeholder="Checkbox"
+              onChange={e => setSunday(e.target.checked)}
+            />
+            <div className="slider" />
+          </label>
+        </div>
+        <div>
+          <span>Segunda:</span>
+          <label className="switch">
+            <input
+              name="monday"
+              type="checkbox"
+              placeholder="Checkbox"
+              onChange={e => setMonday(e.target.checked)}
+            />
+            <div className="slider" />
+          </label>
+        </div>
+        <div>
+          <span>Terça:</span>
+          <label className="switch">
+            <input
+              name="tuesday"
+              type="checkbox"
+              placeholder="Checkbox"
+              onChange={e => setTuesday(e.target.checked)}
+            />
+            <div className="slider" />
+          </label>
+        </div>
+        <div>
+          <span>Quarta:</span>
+          <label className="switch">
+            <input
+              name="wednesday"
+              type="checkbox"
+              placeholder="Checkbox"
+              onChange={e => setWednesday(e.target.checked)}
+            />
+            <div className="slider" />
+          </label>
+        </div>
+        <div>
+          <span>Quinta:</span>
+          <label className="switch">
+            <input
+              name="thursday"
+              type="checkbox"
+              placeholder="Checkbox"
+              onChange={e => setThursday(e.target.checked)}
+            />
+            <div className="slider" />
+          </label>
+        </div>
+        <div>
+          <span>Sexta:</span>
+          <label className="switch">
+            <input
+              name="friday"
+              type="checkbox"
+              placeholder="Checkbox"
+              onChange={e => setFriday(e.target.checked)}
+            />
+            <div className="slider" />
+          </label>
+        </div>
+        <div>
+          <span>Sábado:</span>
+          <label className="switch">
+            <input
+              name="saturday"
+              type="checkbox"
+              placeholder="Checkbox"
+              onChange={e => setSaturday(e.target.checked)}
+            />
+            <div className="slider" />
+          </label>
+        </div>
+      </ul>
       <hr />
-      <button type="submit">Atualizar Dias de Funcionamento</button>
+      <button type="button" onClick={handleSubmit}>
+        Atualizar Dias de Funcionamento
+      </button>
     </Container>
   );
 }
--- desktop/src/pages/Configuration/styles.js
+++ desktop/src/pages/Configuration/styles.js
@@ -5,6 +5,10 @@ export const Container = styled.div`
   max-width: 600px;
   margin: 30px auto;
 
+
+
+
+
   strong{
     display: flex;
     color: #fff;
@@ -12,7 +16,13 @@ export const Container = styled.div`
     justify-content: center;
     margin: 0 15px;
   }
-  
+  aside {
+    justify-self: right;
+  span {
+      margin: 15px;
+      color: rgba(255, 255, 255, 0.7);
+      font-size: 14px;
+  }
   input {
     background: rgba(0, 0, 0, 0.1);
     border: 0;
@@ -27,17 +37,18 @@ export const Container = styled.div`
       color: rgba(255, 255, 255, 0.7);
     }
   }
+}
 
-  form {
+  ul {
     display: flex;
-    margin-top: 20px;
+    margin-top: 8px;
     flex-direction: column;
 
     span {
       color: #e14169;
       align-self: flex-start;
-      margin: 0 0 10px;
       font-weight: bold;
+      margin: 0px;
     }
 
     div {
@@ -57,54 +68,6 @@ export const Container = styled.div`
         margin-left: 20px;
 
       }
-
-      .switch {
-        position: relative;
-        display: inline-block;
-        width: 60px;
-        height: 34px;
-        outline: none;
-      }
-      .switch input {
-        position: absolute;
-        top: -99999px;
-        left: -99999px;
-      }
-      .slider {
-        position: absolute;
-        cursor: pointer;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: #ccc;
-        -webkit-transition: 0.4s;
-        transition: 0.4s;
-        border-radius: 34px;
-      }
-      .slider:before {
-        position: absolute;
-        content: '';
-        height: 26px;
-        width: 26px;
-        left: 4px;
-        bottom: 4px;
-        background-color: white;
-        -webkit-transition: 0.4s;
-        transition: 0.4s;
-        border-radius: 50%;
-      }
-      input:checked + .slider {
-        background-color: #69e141;
-      }
-      input:focus + .slider {
-        box-shadow: 0 0 1px #69e141;
-      }
-      input:checked + .slider:before {
-        -webkit-transform: translateX(26px);
-        -ms-transform: translateX(26px);
-        transform: translateX(26px);
-      }
     }
   }
 
@@ -112,6 +75,7 @@ export const Container = styled.div`
 
   button {
       margin: 5px 0 0;
+      min-width: 600px;
       height: 36px;
       /*background: #e1b941;*/
       background: #3b9eff;
@@ -134,5 +98,5 @@ export const Container = styled.div`
       background: rgba(255, 255, 255, 0.2);
       margin: 10px 0 20px;
     }
-  
+
 `;
